13867071d9fee19037a9604d0d94c2dadea6d835
Alex <berezhnov@gmail.com>
2023-12-24 11:12:50 +0300

    Initial commit

---
+++ src/main/java/pro/sky/kalculyator/KalculyatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.kalculyator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalculyatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalculyatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/kalculyator/KalculyatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.kalculyator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalculyatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
